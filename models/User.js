c9c2cf1f778435dec943455e4874c72e9941e93c
kalejaiyeoluwadara <kalejaiyeoluwadara1@gmail.com>
2024-07-14 21:30:34 +0100

    auth

---
+++ models/User.js
@@ -0,0 +1,43 @@
+const mongoose = require("mongoose");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const UserSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, "Please provide name"],
+    minlength: 3,
+    maxlength: 50,
+  },
+  email: {
+    type: String,
+    required: [true, "Please provide email"],
+    match: [
+      /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
+      "please provide valid mail",
+    ],
+    unique: true,
+  },
+  name: {
+    type: String,
+    required: [true, "Please provide name"],
+    minlength: 3,
+    maxlength: 50,
+  },
+  password: {
+    type: String,
+    required: [true, "Please provide password"],
+    minlength: 6,
+  },
+});
+
+UserSchema.pre("save", async function (next) {
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
+  next();
+});
+UserSchema.methods.createJWT = function () {
+  return jwt.sign({ userId: this._id, name: this.name }, "jwtSecret", {
+    expiresIn: "30d",
+  });
+};
+module.exports = mongoose.model("User", UserSchema);
---
+++ routes/jobs.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const router = express.Router();
+const {
+  getAllJobs,
+  getJob,
+  deleteJob,
+  updateJob,
+  createJob,
+} = require("../controllers/jobs");
+router.route("/").post(createJob).get(getAllJobs);
+router.route("/:id").get(getJob).delete(deleteJob).patch(updateJob);
+
+module.exports = router;
