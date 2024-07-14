6f32da9f455dff97c483caf4e58866a2e1c3296b
kalejaiyeoluwadara <kalejaiyeoluwadara1@gmail.com>
2024-07-14 21:57:09 +0100

    login

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,4 +1,4 @@
-const { BadRequestError } = require("../errors");
+const { BadRequestError, UnauthenticatedError } = require("../errors");
 const User = require("../models/User");
 const { StatusCodes } = require("http-status-codes");
 const register = async (req, res) => {
@@ -8,7 +8,20 @@ const register = async (req, res) => {
 };
 
 const login = async (req, res) => {
-  res.send("login user");
+  const { email, password } = req.body;
+  if (!email || !password) {
+    throw new BadRequestError("Please provide email and pass");
+  }
+  const user = await User.findOne({ email });
+  if (!user) {
+    throw new UnauthenticatedError("Invalid credentials");
+  }
+  const isPasswordCorrect = await user.comparePassword(password);
+  if (!isPasswordCorrect) {
+    throw new UnauthenticatedError("Invalid Credentials");
+  }
+  const token = user.createJWT();
+  res.status(StatusCodes.OK).json({ user: { name: user.name }, token });
 };
 
 module.exports = {
--- models/User.js
+++ models/User.js
@@ -36,8 +36,17 @@ UserSchema.pre("save", async function (next) {
   next();
 });
 UserSchema.methods.createJWT = function () {
-  return jwt.sign({ userId: this._id, name: this.name }, "jwtSecret", {
-    expiresIn: "30d",
-  });
+  return jwt.sign(
+    { userId: this._id, name: this.name },
+    process.env.JWT_SECRET,
+    {
+      expiresIn: process.env.JWT_LIFETIME,
+    }
+  );
+};
+
+UserSchema.methods.comparePassword = async function (candidatePassword) {
+  const isMatch = await bcrypt.compare(candidatePassword, this.password);
+  return isMatch;
 };
 module.exports = mongoose.model("User", UserSchema);
